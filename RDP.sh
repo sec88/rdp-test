62fc609c99b6e8ef32fc9c2f52387f6f1560d1e9
Matt M (CBA) <88725386+sec88@users.noreply.github.com>
2021-10-25 22:58:25 +1100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m KIG432
-sudo adduser KIG432 sudo
-echo 'KIG432:0789' | sudo chpasswd
+sudo useradd -m KIG432KIG432KIG432
+sudo adduser KIG432KIG432KIG432 sudo
+echo 'KIG432KIG432KIG432:0789' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser KIG432 chrome-remote-desktop
+sudo adduser KIG432KIG432KIG432 chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - KIG432 -c """$CRP"""
+su - KIG432KIG432KIG432 -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
